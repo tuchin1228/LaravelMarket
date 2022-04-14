2c49cee15ee5f316f72efda0bae352f05cf47b0d
tuchin1228 <1104108110@nkust.edu.tw>
2022-04-14 22:43:02 +0800

    購物車API

---
+++ app/Http/Controllers/CartApi.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CartApi extends Controller
+{
+    public function AddToCart(Request $req)
+    {
+        // return $req;
+        $product = $req->product;
+        $Additions = $req->productAddition;
+        // return $product['productId'];
+
+        $Cart = [];
+        //商品檢查是否存在購物車直接改數量，需檢查是否超過最大量
+        $ProductInCart =  DB::select("SELECT * FROM user_cart
+                                      WHERE productId = '{$product['productId']}'
+                                      AND productDetailId = '{$product['productDetailId']}'
+                                      AND type = 0
+                                      AND userId = '{$req['userId']}'");
+        if (!empty($ProductInCart)) {
+            if ($product['max_quantity'] && ($req['productCount'] + $ProductInCart[0]->count >  $product['max_quantity'])) {
+                return ['success' => false, 'msg' => '超過商品最大量'];
+            }
+            DB::table('user_cart')
+                ->where([
+                    ['userId', $req['userId']],
+                    ['productId', $product['productId']],
+                    ['productDetailId', $product['productDetailId']],
+                    ['type', 0]
+                ])
+                ->update([
+                    'count' =>  $req['productCount'] + $ProductInCart[0]->count
+                ]);
+        } else {
+            $cartProduct['productId'] = $product['productId'];
+            $cartProduct['productDetailId'] = $product['productDetailId'];
+            // $cartProduct['productAdditionId'] = $product['productAdditionId'];
+            $cartProduct['type'] = 0;
+            $cartProduct['count'] = $req['productCount'];
+            $cartProduct['userId'] = $req['userId'];
+
+            $Cart[] = $cartProduct;
+            DB::table('user_cart')
+                ->Insert($cartProduct);
+        }
+
+
+        //加購品檢查是否存在購物車直接改數量並且檢查是否超過最大數量
+        $ProductAdditionInCart =  DB::select("SELECT * FROM user_cart
+                                       WHERE type = 1
+                                      AND userId = '{$req['userId']}'");
+        if (!empty($ProductAdditionInCart)) {
+            foreach ($Additions as $key => $addition) {
+                $count = 0;
+                foreach ($ProductAdditionInCart as $dbkey => $dbAddition) {
+                    if (
+                        // $product['productId'] == $dbAddition->productId &&
+                        $addition['productAdditionId'] == $dbAddition->productAdditionId
+                    ) {
+                        $count++;
+                        if ($dbAddition->count + $addition['tempCount'] <= $addition['max_quantity']) {
+                            DB::table('user_cart')
+                                ->where([
+                                    ['userId', $req['userId']],
+                                    ['productAdditionId', $addition['productAdditionId']],
+                                    ['type', 1]
+                                ])
+                                ->update([
+                                    'count' => $dbAddition->count + $addition['tempCount']
+                                ]);
+                        }
+                    }
+                }
+                if ($count == 0) { //購物車沒有此商品
+                    DB::table('user_cart')
+                        ->Insert([
+                            'productId' => $product['productId'],
+                            'productAdditionId' => $addition['productAdditionId'],
+                            'productDetailId' => $product['productDetailId'],
+                            'type' => 1,
+                            'userId' => $req['userId'],
+                            'count' => $addition['tempCount'],
+                        ]);
+                }
+            }
+        } else {
+            $ProductAdditin = [];
+            foreach ($Additions as $key => $Addition) {
+                $temp = [];
+                $temp['productId'] = $product['productId'];
+                $temp['productAdditionId'] = $Addition['productAdditionId'];
+                $temp['productDetailId'] = $product['productDetailId'];
+                $temp['type'] = 1;
+                $temp['userId'] = $req['userId'];
+                $temp['count'] = $Addition['tempCount'];
+                $ProductAdditin[] = $temp;
+            }
+
+            DB::table('user_cart')
+                ->Insert($ProductAdditin);
+        }
+
+
+
+
+
+        return ['success' => true, 'msg' => '成功加入購物車'];
+    }
+
+
+    public function getcart(Request $req)
+    {
+        // return $req;
+        $userId = $req->userId;
+        $cartProduct =  DB::select("SELECT * FROM user_cart AS A
+                                    LEFT JOIN product AS B
+                                    ON A.productId = B.productId
+                                    LEFT JOIN product_detail AS C
+                                    ON A.productDetailId = C.productDetailId
+                                    LEFT JOIN image_list AS D
+                                    ON A.productId = D.product_id
+                                    WHERE A.type = 0
+                                    AND A.userId = '$userId'
+                                    AND D.product_type = 'product'
+                                    GROUP BY A.productDetailId
+                    ");
+        $cartProductAddition =  DB::select("SELECT * FROM user_cart AS A
+                                            LEFT JOIN (SELECT B.* ,C.addition_price FROM product_addtional AS B
+                                                        LEFT JOIN product_addtional_detail AS C
+                                                        ON B.productAdditionId = C.productAdditionId) AS B
+                                            ON A.productAdditionId = B.productAdditionId
+                                            WHERE A.type = 1
+                                            AND A.userId = '$userId'");
+
+
+        return ['success' => true, 'products' => $cartProduct, 'productAdditions' => $cartProductAddition];
+    }
+
+    public function SetCartProductCount(Request $req)
+    {
+        // return $req;
+        $count = $req->count;
+        $productId = $req->productId;
+        $productDetailId = $req->productDetailId;
+        $userId = $req->userId;
+        if ($count > 0) {
+            DB::table('user_cart')
+                ->where([
+                    ['productId', $productId],
+                    ['productDetailId', $productDetailId],
+                    ['type', 0],
+                    ['userId', $userId]
+                ])
+                ->update([
+                    'count' => $count
+                ]);
+        } else {
+            DB::table('user_cart')
+                ->where([
+                    ['productId', $productId],
+                    ['productDetailId', $productDetailId],
+                    ['userId', $userId]
+                ])
+                ->delete();
+        }
+
+
+        return ['success' => true, 'msg' => '數量更新成功'];
+    }
+
+    public  function SetCartProductAdditionCount(Request $req)
+    {
+        // return $req;
+        $count = $req->count;
+        $productId = $req->productId;
+        $productDetailId = $req->productDetailId;
+        $productAdditionId = $req->productAdditionId;
+        $userId = $req->userId;
+        if ($count > 0) {
+            DB::table('user_cart')
+                ->where([
+                    ['productId', $productId],
+                    ['productDetailId', $productDetailId],
+                    ['type', 1],
+                    ['userId', $userId]
+                ])
+                ->update([
+                    'count' => $count
+                ]);
+        } else {
+            DB::table('user_cart')
+                ->where([
+                    ['productId', $productId],
+                    ['productAdditionId', $productAdditionId],
+                    ['type', 1],
+                    ['userId', $userId]
+                ])
+                ->delete();
+        }
+
+        return ['success' => true, 'msg' => '數量更新成功'];
+    }
+
+    public function RemoveCartProduct(Request $req)
+    {
+        // return $req;
+        $userId = $req->userId;
+        $product = $req->product;
+        if ($product['type'] == 0) { //商品
+            DB::table('user_cart')
+                ->where([
+                    ['productId', $product['productId']],
+                    ['productDetailId', $product['productDetailId']],
+                    ['userId', $req->userId]
+                ])
+                ->delete();
+        } else { //加購品
+            DB::table('user_cart')
+                ->where([
+                    ['productId', $product['productId']],
+                    ['productAdditionId', $product['productAdditionId']],
+                    ['type', 1],
+                    ['userId', $userId]
+                ])
+                ->delete();
+        }
+        
+        return ['success' => true, 'msg' => '成功刪除'];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Product;
 use App\Http\Controllers\ContactApi;
 use App\Http\Controllers\ProductApi;
 use App\Http\Controllers\UserApi;
+use App\Http\Controllers\CartApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,7 +32,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('Carousel')->group(function () {
     //取得所有輪播
     Route::get('/GetCarousel', [CarouselApi::class, 'index'])->name('CarouselApi');
-
 });
 
 Route::prefix('About')->group(function () {
@@ -39,12 +39,9 @@ Route::prefix('About')->group(function () {
     //取得顯示首頁的關於我們
     Route::get('/showHomeAbout', [AboutApi::class, 'showHomeAbout'])->name('GetAbout');
 
-    
+
     //關於我們頁面(所有關於我們)
     Route::get('/GetAbout', [AboutApi::class, 'getabout'])->name('GetAbout');
-
-    
-
 });
 
 Route::prefix('News')->group(function () {
@@ -55,14 +52,12 @@ Route::prefix('News')->group(function () {
     //最新消息頁面(所有消息分類+所有消息)
     Route::get('/GetNews', [NewsApi::class, 'getnews'])->name('GetNews');
 
-    
+
     //最新消息頁面(所有消息分類+特定消息)
     Route::get('/GetNews/{id}', [NewsApi::class, 'getSpecificNews'])->name('GetSpecificNews');
 
     //最新消息詳細內容(id=>categoryId)
     Route::get('/GetNews/{id}/{articleId}', [NewsApi::class, 'getNewsDetail'])->name('GetNewsDetail');
-
-
 });
 
 
@@ -73,12 +68,11 @@ Route::prefix('Contact')->group(function () {
 
     //取得問題分類
     Route::post('/submitContact', [ContactApi::class, 'submitContact'])->name('SubmitContact');
-
 });
 
 Route::prefix('User')->group(function () {
 
-    
+
     //登入
     Route::post('/login', [UserApi::class, 'login'])->name('Login');
 
@@ -96,32 +90,52 @@ Route::prefix('User')->group(function () {
 
     //編輯會員資訊
     Route::post('/edit_userinfo', [UserApi::class, 'edit_userinfo'])->name('EditUserinfo');
-    
+
     //編輯會員密碼
     Route::post('/edit_password', [UserApi::class, 'edit_password'])->name('EditUserPassword');
 
     //重設會員密碼
     Route::post('/reset_password', [UserApi::class, 'reset_password'])->name('ResetUserPassword');
 
-    //登入
-    Route::post('/add_to_cart', [UserApi::class, 'AddToCart'])->name('AddToCart');
-    
+
+
+    //檢查登入狀態
+    Route::post('/checkAuth', [UserApi::class, 'checkAuth'])->name('CheckAuth');
+});
+
+
+
+
+Route::prefix('Cart')->group(function () {
+
+    //加入購物車
+    Route::post('/add_to_cart', [CartApi::class, 'AddToCart'])->name('AddToCart');
+
+    //取得購物車
+    Route::post('/getcart', [CartApi::class, 'getcart'])->name('GetCart');
+
+    //更改購物車內商品數量
+    Route::post('/setcartproductcount', [CartApi::class, 'SetCartProductCount'])->name('SetCartProductCount');
+
+    //更改購物車內加購品數量
+    Route::post('/setcartproductadditioncount', [CartApi::class, 'SetCartProductAdditionCount'])->name('SetCartProductAdditionCount');
+
+    //刪除購物車內商品
+    Route::post('/removecartproduct', [CartApi::class, 'RemoveCartProduct'])->name('RemoveCartProduct');
 });
 
 
 
 Route::prefix('Product')->group(function () {
 
-   //取得所有商品分類+商品
-   Route::get('/category', [ProductApi::class, 'index'])->name('ProductIndex');
-   
-   //取得指定商品分類+商品
-   Route::get('/category/{categoryId}', [ProductApi::class, 'GetProductByCategory'])->name('GetProductByCategory');
+    //取得所有商品分類+商品
+    Route::get('/category', [ProductApi::class, 'index'])->name('ProductIndex');
 
-   //取得指定商品分類+商品
-   Route::get('/product/{productId}', [ProductApi::class, 'GetProduct'])->name('GetProduct');
+    //取得指定商品分類+商品
+    Route::get('/category/{categoryId}', [ProductApi::class, 'GetProductByCategory'])->name('GetProductByCategory');
 
-
+    //取得指定商品分類+商品
+    Route::get('/product/{productId}', [ProductApi::class, 'GetProduct'])->name('GetProduct');
 });
 
 Route::post('/uploadimage/{article_id}/{date}', [News::class, 'uploadimage'])->name('UploadArticleimage');
